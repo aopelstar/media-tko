9b8a7129e6f8a9788783013e0cf62d322d1b226c
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-06-05 15:04:21 -0600

    companents and routes made

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter, Switch, Route} from 'react-router-dom';
+import About from './components/about/About';
+import Home from './components/home/Home';
+import Packages from './components/packages/Packages';
+import Splash from './components/splash/Splash';
+import Work from './components/work/Work';
+
+export default (
+    <HashRouter>
+        <Switch>
+        </Switch>
+    </HashRouter>
+
+)
