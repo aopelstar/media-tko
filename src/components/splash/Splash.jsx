87deb15ecb0f3ebdbe421b32450dfe093333d40a
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-07-24 23:22:58 -0600

    updated the visibility of the hamburger menu.  Put some finishing touches on before upload.

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -9,7 +9,7 @@ export default function Splash(){
             <Video />
             <img src="https://res.cloudinary.com/media-tko/image/upload/v1528257711/logos/tko_tagline_gold.png" alt="logo" className="splash-logo" />
             <div className="splash-button-container">
-               <Link to='/work'> <button className="splash-button">Get It</button></Link>
+               <Link to='/about'> <button className="splash-button">Get It</button></Link>
             </div>
         </div>
     )
